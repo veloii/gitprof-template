c2352cac60cc646f28212066836d9cf433eeb930
veloii <85405932+veloii@users.noreply.github.com>
2023-10-04 18:03:48 +0100

    fix typing and fix links

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ const config = require("./config.json")
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	important: true,
+	darkMode: "class",
 	theme: {
 		extend: {
 			fontFamily: {
